3365ed5fc4beabb9ccd59291ad7de7f400cb9888
Marc Espie <espie@nerim.net>
2021-04-25 18:02:20 +0200

    data about reset, plus timestamp in 2nd sheet

--- onedit.js
+++ onedit.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Marc Espie <espie@openbsd.org>
+// Copyright (c) 2020-2021 Marc Espie <espie@openbsd.org>
 // 
 // Permission to use, copy, modify, and distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
@@ -19,26 +19,44 @@ function onEdit(e) {
   var ts_range = s.getRange("timestamp"); // ranges belong to a given sheet
   if (ts_range.getSheet().getName() == s.getName()) {
     var c = s.getActiveCell();
-    var col = c.getColumn();
-    var row = c.getRow();
-    
-    var names = ["Stock", "Preparation"];
-    // XXX we assume those ranges are on the same page!
-    var xstamp = ts_range.getColumn();
-      names.forEach(
+    // XXX we assume all those ranges are on the same page!
+    ["Stock", "Preparation"].forEach(
         function(name) {
-          var range = s.getRange(name);
-          var xmin = range.getColumn();
-          var xmax = xmin + range.getWidth();
-          var ymin = range.getRow();
-          var ymax = ymin + range.getHeight();
-          if (col >= xmin && col < xmax && row >= ymin && row <= ymax) {
-            // find where we live in timestamp
-            var cell = c.offset(ts_range.getRow()-ymin, xstamp-col);
-            cell.setValue(new Date());
-          }
+          stampTime(ts_range, c, s.getRange(name));
         });
-    }
-  
+    
+    stampTime(s.getRange("badgeTimestamps"), c, s.getRange("Badges")); 
+  }
 }
+
+// internal function: 
+//      - range to stamp
+//      - active cell
+//      - range to look at
+function stampTime(stampRange, c, range) {
+  var col = c.getColumn();
+  var row = c.getRow();
+  var xmin = range.getColumn();
+  var xmax = xmin + range.getWidth();
+  var ymin = range.getRow();
+  var ymax = ymin + range.getHeight();
+  if (col >= xmin && col < xmax && row >= ymin && row <= ymax) {
+    // do we have a multiple column range, or just one column ?
+    var x = (stampRange.getWidth() > 1) ? xmin : col;
+    // find where we live in timestamp        
+    var cell = c.offset(stampRange.getRow()-ymin, stampRange.getColumn()-x);
+    cell.setValue(new Date());
+  }
+}
+    
+function myReset() {
+  var s = SpreadsheetApp.getActiveSheet();
+  ["aNettoyer1", "aNettoyer2", "aNettoyer3", "aNettoyer4", "aNettoyer5", "aNettoyer6"].forEach(
+    function(n) {
+      var range = s.getRange(n);
+      range.clearContent();
+    });
+}
+    
+
 
